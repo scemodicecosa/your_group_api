692a4f282071e96bffe7f324e3b3b19f3a9811ab
Leonardo <leonardosagratella@gmail.com>
2018-04-27 23:50:59 +0200

    Added comment on API

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GroupsController < ApplicationController
 
-  before_action :authenticate_with_token!, only: [:show, :create, :add_user]
+  before_action :authenticate_with_token!, only: [:show, :create, :add_user,:remove_user]
   respond_to :json
 
 
@@ -19,6 +19,14 @@ class Api::V1::GroupsController < ApplicationController
     end
   end
 
+  '''
+    POST /api/v1/groups/
+    Create a group and add the current_user as admin
+    params { group: {name (pefforza) , description}
+    returns 201 when successfully created new group
+    return 400 when is not created
+  '''
+
   def create
     group = Group.new(group_params)
     if group.save
@@ -29,13 +37,21 @@ class Api::V1::GroupsController < ApplicationController
     end
   end
 
+
+  '''
+    POST /api/v1/groups/add_user
+    Add the user_id to group_id, if admin set the user to admin
+    params { user_id, group_id, admin=false}
+    returns 201 when added the user
+    returns 401 if current_user is not an admin for the group
+  '''
   def add_user
     role = Role.where(group_id: params[:group_id], user: current_user).first
     if role && role.admin
       if params[:admin].to_s == "true"
         role.group.add_user(params[:user_id], true )
       else
-        role.group.add_user(params[:user_id], false)
+        role.group.add_user(params[:user_id])
       end
 
     else
@@ -43,6 +59,13 @@ class Api::V1::GroupsController < ApplicationController
     end
   end
 
+  '''
+    POST /api/v1/groups/remove_user
+    Remove user_id from group_id, if current_user is admin or user_id equals current_user
+    params { user_id, group_id}
+    returns 201 when added the user
+    returns 401 if current_user is not an admin for the group or the one to delete
+  '''
   def remove_user
     role = Role.where(group_id: params[:group_id], user: current_user).first
     if role && (role.admin || params[:user_id].to_i == current_user.id)
